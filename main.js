542ab91522e3afa0a0ef15174bb5c77e5d431e98
Dawud Rehan <d.rehan7@gmail.com>
2021-02-03 17:35:52 -0500

    conversion works and api request is good. just need to do styling

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 let dropDowns = document.querySelectorAll("select");
-
+let convertButton = document.querySelector("button");
+let url = "https://api.exchangeratesapi.io/latest?base="
 
 
 for (let dropDown of dropDowns) {
     dropDown.addEventListener("change", changeCurrencyLabels);
 }
 
+convertButton.addEventListener("click", buttonEvent);
 
 
 
@@ -21,7 +23,35 @@ changeCurrencyLabels()
 
 
 
+function buttonEvent() {
+    let baseCurrency = document.querySelector("#base-currency-dropdown").value;
+    let targetCurrency= document.querySelector("#target-currency-dropdown").value;
+    let baseAmount = parseInt(document.querySelector("input").value);
+    let resultLabel = document.querySelector(".result-currency");
 
+    let result;
+
+    fetch(url+baseCurrency)
+        .then(response => {
+            return response.json()
+        })
+        .then(data => {
+            const conversionRate = data.rates[targetCurrency];
+            result = calculateConversion(baseAmount, conversionRate);
+            resultLabel.textContent = formatResultCurrency(result);
+        })
+
+    
+}
+
+
+function calculateConversion(amount, conversionRate) {
+    return amount * conversionRate
+}
+
+function formatResultCurrency(result) {
+    return `$${result.toFixed(2)}`
+}
 
 
 
